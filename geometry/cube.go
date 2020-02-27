b12a660c0fe44a0bf9c856f0ce422212193e6081
vishal.gupta <vishal.gupta@softminesol.com>
2020-02-27 15:20:02 +0530

    import errors

--- geometry/cube.go
+++ geometry/cube.go
@@ -6,5 +6,5 @@ func CubeVolume(n int) (int ,error) {
 	if n!=0 {
 		return n*n*n , nil
 	}
-	return 0, errors.New("Zero length is not allowed")
+	return 0, errors.New("zero value is not allowed")
 }
