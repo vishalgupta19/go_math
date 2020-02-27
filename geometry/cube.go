f27bed53c137e857886881382f54fc503256fdf9
vishal.gupta <vishal.gupta@softminesol.com>
2020-02-27 15:04:28 +0530

    zero length not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,9 @@
 package geometry
 
-func CubeVolume(n int) int {
-	return n*n
+func CubeVolume(n int) (int ,error) {
+
+	if n!=0 {
+		return n*n*n , nil
+	}
+	return 0, errors.new("Zero length is not allowed")
 }
